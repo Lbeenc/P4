b571a88c7791e3445552bebeb5ab2451bb919a3a
Lbeenc <Lbeenc@gmail.com>
2025-12-19 17:44:45 -0600

    Update codeGen.h

--- codeGen.h
+++ codeGen.h
@@ -1,34 +1,10 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <fstream>
+#include <ostream>
 #include "node.h"
 
-class CodeGen {
-public:
-    CodeGen(const std::string& outFile);
-    void generate(node* root);
-
-private:
-    std::ofstream out;
-
-    std::vector<std::string> storage;
-    std::vector<std::string> code;
-
-    std::unordered_set<std::string> declared;
-
-    int tempCount = 0;
-
-    std::string newTemp();
-    void declare(const std::string& name);
-
-    void emitCode(const std::string& s);
-    void emitStorage(const std::string& s);
-
-    void traverse(node* n);
-};
+// NOTE: Node is capitalized (matches node.h)
+void generateTarget(Node* root, std::ostream& out);
 
 #endif

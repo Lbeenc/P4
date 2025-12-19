da1f9cf5ef48897640fbd3e9aa203c759b143c38
Lbeenc <Lbeenc@gmail.com>
2025-12-19 15:37:32 -0600

    Update codegen.h

--- codegen.h
+++ codegen.h
@@ -1,11 +1,11 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include <string>
 #include "node.h"
+#include <string>
+#include <fstream>
 
-// Generate assembly for the whole program parse tree.
-// outFile should be "a.asm" (keyboard input) or "<file>.asm" (file input).
+// entry point from main
 void generateTarget(Node* root, const std::string& outFile);
 
 #endif

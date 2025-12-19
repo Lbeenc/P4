696e8f8efc54e96853e4360a5d6248762a4aeeba
Lbeenc <Lbeenc@gmail.com>
2025-12-19 17:38:57 -0600

    Update codeGen.h

--- codeGen.h
+++ codeGen.h
@@ -1,10 +1,34 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include <ostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <fstream>
 #include "node.h"
 
-// Generate target assembly into the provided output stream.
-void generateTarget(Node* root, std::ostream& out);
+class CodeGen {
+public:
+    CodeGen(const std::string& outFile);
+    void generate(node* root);
+
+private:
+    std::ofstream out;
+
+    std::vector<std::string> storage;
+    std::vector<std::string> code;
+
+    std::unordered_set<std::string> declared;
+
+    int tempCount = 0;
+
+    std::string newTemp();
+    void declare(const std::string& name);
+
+    void emitCode(const std::string& s);
+    void emitStorage(const std::string& s);
+
+    void traverse(node* n);
+};
 
 #endif
